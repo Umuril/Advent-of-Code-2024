43f1e6c068fa2fcf9a8836395cb2b7f6f91edd76
Umuril Lyerood <umuril.lyerood@gmail.com>
2024-12-17 10:49:32 -0500

    Add fifteenth day solution

---
+++ src/bin/15.rs
@@ -0,0 +1,92 @@
+use core::str;
+
+use advent_of_code::{Matrix, DOWN, LEFT, RIGHT, UP};
+
+advent_of_code::solution!(15);
+
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (part_a, part_b) = input.trim().split_once("\n\n").expect("Correct input format");
+    let nrows = part_a.as_bytes().iter().filter(|&&c| c == b'\n').count() + 1;
+    let ncols = part_a.find('\n').unwrap();
+    let part_a = &part_a.replace('\n', "");
+    let mut matrix = Matrix::from(nrows, ncols, part_a.as_bytes().to_vec());
+    // println!("{}", matrix);
+
+    let part_b = &part_b.replace('\n', "");
+    let moves : Vec<u8> = part_b.as_bytes().to_vec();
+    // println!("{:?}", moves);
+
+    let mut guard_pos = matrix.find(&b'@').expect("Expect one guard");
+
+    for mov in moves {
+        let direction = match mov {
+            b'^' => UP,
+            b'v' => DOWN,
+            b'<' => LEFT,
+            b'>' => RIGHT,
+            _ => unreachable!(),
+        };
+
+        let mut next_pos = guard_pos + direction.as_point();
+
+        // println!("GUARD: {} - NEXT: {} - DIRECTION: {}", guard_pos, next_pos, direction);
+
+        while let Some(next_byte) = matrix.get(&next_pos) {
+            // println!("NEXT BYTE: {}", str::from_utf8(&[*next_byte]).unwrap());
+            if *next_byte == b'#' {
+                break;
+            }
+            if *next_byte == b'O' {
+                next_pos += direction.as_point();
+                continue;
+            }
+            if *next_byte == b'.' {
+                while next_pos != guard_pos {
+                    let last_point = next_pos + direction.opposite_point();
+                    let old_chr = matrix.get(&last_point).unwrap();
+                    // println!("LAST: {} - OLD CHR: {}", last_point, old_chr);
+                    matrix.update(&next_pos, *old_chr);
+                    matrix.update(&last_point, b'.');
+
+                    next_pos += direction.opposite_point();
+                }
+                guard_pos += direction.as_point();
+
+                break;
+            }
+        }
+    }
+
+    let mut acc = 0;
+    for p in matrix.as_points() {
+        if let Some(chr) = matrix.get(&p) {
+            if *chr == b'O' {
+                acc += (p.0 * 100 + p.1) as u64;
+            }
+        }
+    }
+
+    Some(acc)
+}
+
+pub fn part_two(_input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(10092));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 pub mod template;
 
-use std::ops::{Add, Mul, Sub};
+use core::str;
+use std::ops::{Add, AddAssign, Mul, Sub, SubAssign};
 
 use std::fmt::Debug;
 use std::fmt::Display;
 use std::fmt::Write;
 
-use forward_ref::forward_ref_binop;
+use forward_ref::{forward_ref_binop, forward_ref_op_assign};
 
 // Use this file to add helper functions and additional modules.
 
@@ -17,7 +18,7 @@ pub struct Matrix<T> {
     pub data: Vec<T>,
 }
 
-impl<T> Matrix<T> {
+impl<T: PartialEq> Matrix<T> {
     pub fn from(rows: usize, cols: usize, data: Vec<T>) -> Matrix<T> {
         assert_eq!(data.len(), rows * cols);
         Matrix {
@@ -53,14 +54,43 @@ impl<T> Matrix<T> {
         let cols = self.cols;
         Box::from((0..rows).flat_map(move |x| (0..cols).map(move |y| Point(x, y))))
     }
+
+    pub fn find(&self, needle: &T) -> Option<Point> {
+        for p in self.as_points() {
+            if let Some(chr) = self.get(&p) {
+                if *chr == *needle {
+                    return Some(p);
+                }
+            }
+        }
+        None
+    }
 }
 
-impl<T: Display> Debug for Matrix<T> {
+impl Display for Matrix<u8> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for x in 0..self.rows {
-            f.write_char('\n')?;
+            if x > 0 {
+                f.write_char('\n')?;
+            }
             for y in 0..self.cols {
                 let value = self.get(&Point(x, y)).expect("Checked");
+                f.write_str(str::from_utf8(&[*value]).unwrap()).unwrap();
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<T: Display + PartialEq> Debug for Matrix<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for x in 0..self.rows {
+            if x > 0 {
+                f.write_char('\n')?;
+            }
+            for y in 0..self.cols {
+                let p = Point(x, y);
+                let value = self.get(&p).expect("Checked");
                 f.write_str(value.to_string().as_str()).unwrap();
             }
         }
@@ -70,6 +100,12 @@ impl<T: Display> Debug for Matrix<T> {
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Point(pub isize, pub isize);
+
+impl Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("({}, {})", self.0, self.1))
+    }
+}
 
 impl Add for Point {
     type Output = Self;
@@ -81,6 +117,16 @@ impl Add for Point {
 
 forward_ref_binop!(impl Add, add for Point, Point);
 
+impl AddAssign for Point {
+    #[inline]
+    fn add_assign(&mut self, other: Self) {
+        self.0 += other.0;
+        self.1 += other.1;
+    }
+}
+
+forward_ref_op_assign!(impl AddAssign, add_assign for Point, Point);
+
 impl Sub for Point {
     type Output = Self;
 
@@ -91,6 +137,16 @@ impl Sub for Point {
 
 forward_ref_binop!(impl Sub, sub for Point, Point);
 
+impl SubAssign for Point {
+    #[inline]
+    fn sub_assign(&mut self, other: Self) {
+        self.0 -= other.0;
+        self.1 -= other.1;
+    }
+}
+
+forward_ref_op_assign!(impl SubAssign, sub_assign for Point, Point);
+
 impl Mul<isize> for Point {
     type Output = Self;
 
@@ -98,3 +154,47 @@ impl Mul<isize> for Point {
         Self(self.0 * factor, self.1 * factor)
     }
 }
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Direction {
+    Up(Point),
+    Down(Point),
+    Left(Point),
+    Right(Point),
+}
+
+impl Direction {
+    pub fn as_point(self) -> Point {
+        match self {
+            Direction::Up(p) => p,
+            Direction::Down(p) => p,
+            Direction::Left(p) => p,
+            Direction::Right(p) => p,
+        }
+    }
+
+    pub fn opposite_point(self) -> Point {
+        match self {
+            Direction::Up(_) => DOWN.as_point(),
+            Direction::Down(_) => UP.as_point(),
+            Direction::Left(_) => RIGHT.as_point(),
+            Direction::Right(_) => LEFT.as_point(),
+        }
+    }
+}
+
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_char(match self {
+            Direction::Up(_) => '^',
+            Direction::Down(_) => 'v',
+            Direction::Left(_) => '<',
+            Direction::Right(_) => '>',
+        })
+    }
+}
+
+pub static UP: Direction = Direction::Up(Point(-1, 0));
+pub static DOWN: Direction = Direction::Down(Point(1, 0));
+pub static LEFT: Direction = Direction::Left(Point(0, -1));
+pub static RIGHT: Direction = Direction::Right(Point(0, 1));
