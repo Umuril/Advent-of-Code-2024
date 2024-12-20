c6c5d2df6603911a796692ec7ebe4c79ca00b4b1
Umuril Lyerood <umuril.lyerood@gmail.com>
2024-12-20 05:50:18 -0500

    Add fifteenth day solution

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,6 +1,6 @@
 use core::str;
 
-use advent_of_code::{Matrix, DOWN, LEFT, RIGHT, UP};
+use advent_of_code::{Direction, Matrix, Point, DOWN, LEFT, RIGHT, UP};
 
 advent_of_code::solution!(15);
 
@@ -72,8 +72,149 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(acc)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+fn is_rock(matrix: &mut Matrix<u8>, pos: &Point) -> bool {
+    let chr = *matrix.get(pos).unwrap();
+
+    chr == b'[' || chr == b']'
+}
+
+fn can_move(direction: Direction, matrix: &mut Matrix<u8>, guard_pos: Point) -> bool {
+    let new_pos = guard_pos + direction.as_point();
+    let new_chr = *matrix.get(&new_pos).unwrap();
+
+    if new_chr == b'.' {
+        return true;
+    }
+
+    if new_chr == b'#' {
+        return false;
+    }
+
+    // []
+    let mut pos = guard_pos + direction.as_point();
+    if direction == LEFT || direction == RIGHT {
+        while let Some(next_byte) = matrix.get(&pos) {
+            if *next_byte == b'#' {
+                return false;
+            }
+            if *next_byte == b'.' {
+                return true;
+            }
+            pos += direction.as_point();
+        }
+    }
+
+    if direction == UP || direction == DOWN {
+        while let Some(next_byte) = matrix.get(&pos) {
+            if *next_byte == b'#' {
+                return false;
+            }
+            if *next_byte == b'.' {
+                return true;
+            }
+            if *next_byte == b'[' && direction == UP {
+                return can_move(direction, matrix, pos) && can_move(direction, matrix, pos + RIGHT.as_point());
+            }
+            if *next_byte == b']' && direction == UP {
+                return can_move(direction, matrix, pos) && can_move(direction, matrix, pos + LEFT.as_point());
+            }
+            if *next_byte == b'[' && direction == DOWN {
+                return can_move(direction, matrix, pos) && can_move(direction, matrix, pos + RIGHT.as_point());
+            }
+            if *next_byte == b']' && direction == DOWN {
+                return can_move(direction, matrix, pos) && can_move(direction, matrix, pos + LEFT.as_point());
+            }
+            pos += direction.as_point();
+        }
+    }
+
+    unreachable!()
+}
+
+fn move_part(direction: Direction, matrix: &mut Matrix<u8>, pos: Point) -> Point {
+    let old_chr = *matrix.get(&pos).unwrap();
+    let new_pos = pos + direction.as_point();
+    // println!("move_part: {direction} {pos}");
+    let new_chr = *matrix.get(&new_pos).unwrap();
+
+    if new_chr == b'.' {
+        matrix.update(&pos, b'.');
+        matrix.update(&new_pos, old_chr);
+        return new_pos;
+    }
+
+    match direction {
+        Direction::Left(_) | Direction::Right(_) => {
+            move_part(direction, matrix, new_pos);
+        },
+        Direction::Up(_) | Direction::Down(_) => {
+            let new_pos_adjacent = match new_chr {
+                b'[' => new_pos + RIGHT.as_point(),
+                b']' => new_pos + LEFT.as_point(),
+                _ => unreachable!()
+            };
+
+            move_part(direction, matrix, new_pos);
+            move_part(direction, matrix, new_pos_adjacent);
+        },
+    }
+
+    matrix.update(&pos, b'.');
+    matrix.update(&new_pos, old_chr);
+
+    return new_pos;
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let (part_a, part_b) = input
+        .trim()
+        .split_once("\n\n")
+        .expect("Correct input format");
+    let nrows = part_a.as_bytes().iter().filter(|&&c| c == b'\n').count() + 1;
+    let ncols = part_a.find('\n').unwrap();
+    let part_a = &part_a
+        .replace('\n', "")
+        .replace('#', "##")
+        .replace('O', "[]")
+        .replace('.', "..")
+        .replace('@', "@.");
+    let mut matrix = Matrix::from(nrows, ncols * 2, part_a.as_bytes().to_vec());
+    // println!("{}", matrix);
+
+    let part_b = &part_b.replace('\n', "");
+    let moves: Vec<u8> = part_b.as_bytes().to_vec();
+    // println!("{:?}", moves);
+
+    let mut guard_pos = matrix.find(&b'@').expect("Expect one guard");
+
+    for mov in moves {
+        let direction = match mov {
+            b'^' => UP,
+            b'v' => DOWN,
+            b'<' => LEFT,
+            b'>' => RIGHT,
+            _ => unreachable!(),
+        };
+
+        // println!("{} {} {}", direction, guard_pos, can_move(direction, &mut matrix, guard_pos));
+
+        if can_move(direction, &mut matrix, guard_pos) {
+            guard_pos = move_part(direction, &mut matrix, guard_pos);
+        }
+
+        // println!("{}", matrix);
+    }
+
+    let mut acc = 0;
+    for p in matrix.as_points() {
+        if let Some(chr) = matrix.get(&p) {
+            if *chr == b'[' {
+                acc += (p.0 * 100 + p.1) as u64;
+            }
+        }
+    }
+
+    Some(acc)
 }
 
 #[cfg(test)]
@@ -89,6 +230,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9021));
     }
 }
